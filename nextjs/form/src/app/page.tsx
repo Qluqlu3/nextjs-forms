68db4eea403bdac1c82b1fbb12ef910a7ef4ee77
Qluqlu3 <plksmcgof@gmail.com>
2023-02-10 00:19:58 +0900

    use grid css

--- nextjs/form/src/app/page.tsx
+++ nextjs/form/src/app/page.tsx
@@ -1,7 +1,11 @@
 export default function Home() {
   return (
-    <main className='flex justify-center items-center p-8'>
+    <main className='flex justify-center flex-col items-center p-8'>
       <h1 className='text-white text-7xl font-bold'>タイトル</h1>
+      <div className='grid grid-cols-2 w-full mt-10'>
+        <div className='flex items-center justify-center w-full text-5xl h-[100px] bg-blue-200'>①</div>
+        <div className='flex items-center justify-center w-full h-[100px] text-5xl bg-blue-400'>②</div>
+      </div>
     </main>
   );
 }
